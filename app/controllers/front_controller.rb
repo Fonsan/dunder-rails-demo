b89ec15c23aa2a6ceb81d4b521ff0ae3efab04d7
Erik Fonselius <fonsan@gmail.com>
2011-02-12 14:57:11 +0100

    Default behavior done

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,7 +1,7 @@
 class FrontController < ApplicationController
   def index
-    Widget.find_by_sql("select sleep(1),'one'")
-    Widget.find_by_sql("select sleep(1),'two'")
+    @one = Widget.find_by_sql("select sleep(1),'one'").first
+    @two = Widget.find_by_sql("select sleep(1),'two'").first
   end
 
 end
